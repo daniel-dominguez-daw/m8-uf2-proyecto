d42d4cfc29afec31b893a06c4691c4ef29d35fa5
AlejandroRO <cf19alejandro.ramos@iesjoandaustria.org>
2021-02-09 18:50:02 +0100

    reestructuring folders and adding basic authorization

--- src/calc.py
+++ src/calc.py
@@ -1,30 +1,36 @@
-import os
+from flask import Flask, jsonify, render_template
+from flask_basicauth import BasicAuth
 
-from flask import Flask, jsonify
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 
-@app.route('/suma/<op1>/<op2>')
+app.config['BASIC_AUTH_USERNAME'] = 'daw'
+app.config['BASIC_AUTH_PASSWORD'] = 'prova'
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+@basic_auth.required
+def access_calc():
+    return render_template('index.html')
+
 def suma(op1, op2):
     n_op1 = float(op1)
     n_op2 = float(op2)
     resultat = {'operador': 'suma','resultat ' : n_op1 + n_op2}
     return jsonify(resultat), 200
 
-@app.route('/resta/<op1>/<op2>')
 def resta(op1, op2):
     n_op1 = float(op1)
     n_op2 = float(op2)
     resultat = {'operador': 'resta','resultat ' : n_op1 - n_op2}
     return jsonify(resultat), 200
 
-@app.route('/multiplicacio/<op1>/<op2>')
 def multiplicacio(op1, op2):
     n_op1 = float(op1)
     n_op2 = float(op2)
     resultat = {'operador': 'multiplicacio','resultat ' : n_op1 * n_op2}
     return jsonify(resultat), 200
 
-@app.route('/divisio/<op1>/<op2>')
 def divisio(op1, op2):
     n_op1 = float(op1)
     n_op2 = float(op2)
@@ -32,4 +38,4 @@ def divisio(op1, op2):
     return jsonify(resultat), 200
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5050')
